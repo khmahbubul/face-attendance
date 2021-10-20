64cec4368feb6eebeb6d113327c97d73cdd1f495
Mahbubul Islam <khmahbub20@gmail.com>
2021-10-20 23:59:05 +0600

    login page added

--- config/fortify.php
+++ config/fortify.php
@@ -84,11 +84,11 @@ return [
     */
 
     'features' => [
-        Features::registration(),
+        // Features::registration(),
         Features::resetPasswords(),
-        Features::emailVerification(),
-        Features::updateProfileInformation(),
-        Features::updatePasswords(),
+        // Features::emailVerification(),
+        // Features::updateProfileInformation(),
+        // Features::updatePasswords(),
 //        Features::twoFactorAuthentication([
 //            'confirmPassword' => true,
 //        ]),
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,128 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en" dir="ltr">
+	<head>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+		<!-- Meta data -->
+		<meta charset="UTF-8">
+		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+		<meta content="Dashmint – Bootstrap Responsive Flat Admin Dashboard HTML5 Template" name="description">
+		<meta content="Spruko Technologies Private Limited" name="author">
+		<meta name="keywords" content="admin bootstrap 4 template, admin dashboard, admin dashboard template, admin panel html template, admin panel template, bootstrap 4 dashboard, modern admin template, bootstrap admin dashboard, bootstrap admin dashboard template, bootstrap admin template, bootstrap dashboard, bootstrap dashboard template, bootstrap simple dashboard, dashboard bootstrap 4, dashboard html css, dashboard template bootstrap 4, bootstrap control panel, template bootstrap 4 admin, simple bootstrap admin template, simple dashboard html template, simple dashboard template bootstrap"/>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+		<!--favicon -->
+		<link rel="icon" href="{{ asset('assets/images/brand/favicon.ico') }}" type="image/x-icon"/>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+		<!-- TITLE -->
+		<title>Dashmint – Bootstrap Responsive Flat Admin Dashboard HTML5 Template.</title>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+		<!-- BOOTSTRAP CSS -->
+		<link href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+		<!-- DASHBOARD CSS -->
+		<link href="{{ asset('assets/css/style.css') }}" rel="stylesheet"/>
+        <link href="{{ asset('assets/css/skins-modes.css') }}" rel="stylesheet"/>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+		<!-- INTERNAL SINGLE-PAGE CSS -->
+		<link href="{{ asset('assets/plugins/single-page/css/single-page.css') }}" rel="stylesheet" type="text/css">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+		<!--- FONT-ICONS CSS -->
+		<link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet"/>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+	    <!-- COLOR SKIN CSS -->
+		<link id="theme" rel="stylesheet" type="text/css" media="all" href="{{ asset('assets/css/color-skins/color11.css') }}" />
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+	</head>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+	<body class="app sidebar-mini">
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+		<!-- BACKGROUND-IMAGE -->
+		<div class="login-img">
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+			<!-- GLOABAL LOADER -->
+			<div id="global-loader">
+				<img src="{{ asset('assets/images/svgs/loader.svg') }}" class="loader-img" alt="Loader">
+			</div>
+
+			<div class="page h-100">
+				<div class="">
+				    <!-- CONTAINER OPEN -->
+					<div class="col col-login mx-auto">
+						<div class="text-center">
+							<img src="{{ asset('assets/images/brand/logo.png') }}" class="header-brand-img" alt="">
+						</div>
+					</div>
+					<div class="container-login100">
+						<div class="wrap-login100 p-6">
+							<form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
+								@csrf
+								<span class="login100-form-title">
+									Member Login
+								</span>
+								<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+									<input class="input100 @error('email') is-invalid @enderror" type="email" name="email" placeholder="Email" value="{{ old('email') }}" required autocomplete="email">
+									<span class="focus-input100"></span>
+									<span class="symbol-input100">
+										<i class="zmdi zmdi-email" aria-hidden="true"></i>
+									</span>
+									@error('email')
+										<span class="invalid-feedback" role="alert">
+											<strong>{{ $message }}</strong>
+										</span>
+                                	@enderror
+								</div>
+								<div class="wrap-input100 validate-input" data-validate = "Password is required">
+									<input class="input100 @error('password') is-invalid @enderror" type="password" name="password" placeholder="Password" required>
+									<span class="focus-input100"></span>
+									<span class="symbol-input100">
+										<i class="zmdi zmdi-lock" aria-hidden="true"></i>
+									</span>
+									@error('password')
+										<span class="invalid-feedback" role="alert">
+											<strong>{{ $message }}</strong>
+										</span>
+                                	@enderror
+								</div>
+
+								@if (Route::has('password.request'))
+									<div class="text-right pt-1">
+										<p class="mb-0"><a href="{{ route('password.request') }}" class="text-primary ml-1">Forgot Password?</a></p>
+									</div>
+								@endif
+
+								<div class="container-login100-form-btn">
+									<button type="submit" class="login100-form-btn btn-primary">
+										Login
+									</button>
+								</div>
+							</form>
+						</div>
+					</div>
+					<!-- CONTAINER CLOSED -->
+				</div>
+			</div>
+		</div>
+		<!-- BACKGROUND-IMAGE CLOSED -->
+
+		<!-- JQUERY SCRIPTS JS-->
+		<script src="{{ asset('assets/js/vendors/jquery-3.2.1.min.js') }}"></script>
+
+		<!-- BOOTSTRAP SCRIPTS JS-->
+		<script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+		<!-- SPARKLINE JS-->
+		<script src="{{ asset('assets/js/vendors/jquery.sparkline.min.js') }}"></script>
+
+		<!-- CHART-CIRCLE JS-->
+		<script src="{{ asset('assets/js/vendors/circle-progress.min.js') }}"></script>
+
+		<!-- RATING STAR JS-->
+		<script src="{{ asset('assets/plugins/rating/rating-stars.js') }}"></script>
+
+		<!-- INPUT MASK JS-->
+		<script src="{{ asset('assets/plugins/input-mask/input-mask.min.js') }}"></script>
+
+		<!-- CUSTOM JS-->
+		<script src="{{ asset('assets/js/custom.js') }}"></script>
+
+	</body>
+</html>
